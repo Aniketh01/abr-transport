cfc8cd1d7c8540eee51d40a924091e3a6c13cc23
Aniketh01 <anikethgireesh@gmail.com>
2020-05-28 00:05:09 +0530

    Added link spped limit

--- scripts/mininet/butterfly_topology.py
+++ scripts/mininet/butterfly_topology.py
@@ -1,8 +1,10 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel, info
 
+linkopts = dict(bw=30, delay='50ms', loss=10, use_htb=True)
 
 class butterflyTopo(Topo):
     def __init__(self, n=4, **kwargs):
@@ -14,15 +16,15 @@ class butterflyTopo(Topo):
         for h in range(n):
             host_1 = self.addHost('h%s' % (h))
             host_2 = self.addHost('h%s' % (h + 4))
-            self.addLink(host_1, s1)
-            self.addLink(host_2, s2)
-        self.addLink(s1, s2)
+            self.addLink(host_1, s1, **linkopts)
+            self.addLink(host_2, s2, **linkopts)
+        self.addLink(s1, s2, **linkopts)
 
 
 def createNet():
 
     topo = butterflyTopo()
-    net = Mininet(topo)
+    net = Mininet(topo, link=TCLink)
 
     info( "*** Starting network\n" )
     net.start()
@@ -31,6 +33,9 @@ def createNet():
     dumpNodeConnections(net.hosts)
     info("Test connectivity")
     net.pingAll()
+    print "Testing bandwidth between h1 and h4"
+    h1, h4 = net.get( 'h1', 'h4' )
+    net.iperf( (h1, h4) )
     info( "*** Stopping network\n" )
     net.stop()
 
