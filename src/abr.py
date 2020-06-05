ea9cd4cca1ac2f87b6af061ec75a54303e2e6d9b
Aniketh01 <anikethgireesh@gmail.com>
2020-06-06 01:10:56 +0530

    ABR: Added throughput rule

--- src/abr.py
+++ src/abr.py
@@ -5,10 +5,39 @@
 class abr:
     def __init__(self, manifestData):
         self.manifestData = manifestData
-    
+
     def getBitrateList(self):
         manifest_bitrate = self.manifestData.get('bitrates_kbps')
         bitrateList = []
         for bitrate in manifest_bitrate:
             bitrateList.append(int(bitrate))
         return bitrateList
+
+    '''
+    The throughput rule: bitrate is choosen as per the last throughput.
+    The function estimates the quality index for next segment to be downloaded
+    on the basis of the throughput rule.
+    '''
+    def NextSegmentQualityIndex(self, playerStats):
+        tput = playerStats["lastTput"]
+        #p = manifest.segment_time
+        m_bitrate = self.manifestData.get('bitrates_kbps')
+        if not tput:
+            return 0
+
+        quality_idx = 1
+        for bitrate in m_bitrate:
+            idx = m_bitrate.index(bitrate)
+            if bitrate > tput:
+                quality_idx = idx - 1
+                break
+            quality_idx = idx
+
+        if quality_idx >= 1:
+            return quality_idx
+        else:
+            return 0
+
+    def NextSegmentSize(self, segment_idx, quality):
+        size = self.manifestData['segment_size_bytes'][segment_idx][quality]
+        return size
