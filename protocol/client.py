a0ad5db4ad3f3ca32874727e27523c1a6bef093f
Aniketh01 <anikethgireesh@gmail.com>
2020-06-08 23:36:11 +0530

    Protocol: Initial client commit

---
+++ protocol/client.py
@@ -0,0 +1,21 @@
+import asyncio
+import argparse
+import logging
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="HTTP/3 client")
+    parser.add_argument(
+        "url", type=str, nargs="+", help="the URL to query (must be HTTPS)"
+    )
+    parser.add_argument(
+        "--ca-certs", type=str, help="load CA certificates from the specified file"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="increase logging verbosity"
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        level=logging.DEBUG if args.verbose else logging.INFO,
+    )
