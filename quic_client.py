e4820151e0ccbb16787faca173f3c4c10c17949a
Aniketh01 <anikethgireesh@gmail.com>
2020-08-10 19:05:16 +0530

    Clients: refactor

--- quic_client.py
+++ quic_client.py
@@ -8,14 +8,13 @@ from urllib.parse import urlparse
 
 from aioquic.quic.configuration import QuicConfiguration
 from aioquic.quic.events import QuicEvent, DatagramFrameReceived, StreamDataReceived
-from aioquic.tls import SessionTicket
 
 from quic_logger import QuicDirectoryLogger
 
 from protocol.client import connect
 from protocol.socketFactory import QuicFactorySocket
 
-logger = logging.getLogger("client")
+logger = logging.getLogger("quic client")
 
 
 class QuicClient(QuicFactorySocket):
@@ -60,117 +59,3 @@ class QuicClient(QuicFactorySocket):
                 waiter = self._ack_waiter
                 self._ack_waiter = None
                 waiter.set_result(None)
-
-
-async def run(configuration: QuicConfiguration, urls: List[str], zero_rtt: bool) -> None:
-    parsed = urlparse(urls[0])
-    if ":" in parsed.netloc:
-        host, port = parsed.netloc.split(":")
-        port = int(port)
-    else:
-        host = parsed.netloc
-        port = 8080
-
-    async with connect(
-        host, port,
-        configuration=configuration,
-        create_protocol=QuicClient,
-        wait_connected=not zero_rtt,
-    ) as client:
-        client = cast(QuicClient, client)
-        logger.info("sending quic ack")
-        await client.quic_datagram_send()
-        logger.info("recieved quic ack")
-        logger.info("sending quic data in stream")
-        for i in range(5):
-            await client.quic_stream_send()
-        logger.info("recieved quic data in stream")
-
-
-def save_session_ticket(ticket: SessionTicket) -> None:
-    """
-    Callback which is invoked by the TLS engine when a new session ticket
-    is received.
-    """
-    logger.info("New session ticket received")
-    if args.session_ticket:
-        with open(args.session_ticket, "wb") as fp:
-            pickle.dump(ticket, fp)
-
-
-if __name__ == "__main__":
-    defaults = QuicConfiguration(is_client=True)
-    print(defaults)
-    parser = argparse.ArgumentParser(description="QUIC client")
-    parser.add_argument(
-        "url", type=str, nargs="+", help="the URL to query"
-    )
-    parser.add_argument(
-        "--ca-certs", type=str,
-        help="load CA certificates from the specified file"
-    )
-    parser.add_argument(
-        "-k",
-        "--insecure",
-        action="store_true",
-        help="do not validate server certificate",
-    )
-    parser.add_argument(
-        "-i",
-        "--include",
-        action="store_true",
-        help="include the HTTP response headers in the output",
-    )
-    parser.add_argument(
-        "--output-dir", type=str,
-        help="write downloaded files to this directory",
-    )
-    parser.add_argument(
-        "-q",
-        "--quic-log",
-        type=str,
-        help="log QUIC events to QLOG files in the specified directory",
-    )
-    parser.add_argument(
-        "-v", "--verbose", action="store_true",
-        help="increase logging verbosity"
-    )
-    parser.add_argument(
-        "-l",
-        "--secrets-log",
-        type=str,
-        help="log secrets to a file, for use with Wireshark",
-    )
-    parser.add_argument(
-        "--zero-rtt", action="store_true",
-        help="try to send requests using 0-RTT"
-    )
-
-    args = parser.parse_args()
-
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)s %(name)s %(message)s",
-        level=logging.DEBUG if args.verbose else logging.INFO,
-    )
-
-    configuration = QuicConfiguration(
-        alpn_protocols=['quic'], is_client=True, max_datagram_frame_size=65536
-    )
-
-    if args.ca_certs:
-        configuration.load_verify_locations(args.ca_certs)
-    if args.insecure:
-        configuration.verify_mode = ssl.CERT_NONE
-    if args.quic_log:
-        configuration.quic_logger = QuicDirectoryLogger(args.quic_log)
-    if args.secrets_log:
-        configuration.secrets_log_file = open(args.secrets_log, "a")
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        run(
-            configuration=configuration,
-            urls=args.url,
-            zero_rtt=args.zero_rtt,
-        )
-    )
