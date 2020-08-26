9c619c964159570bda7033bfb4dd00ea805b5d01
Aniketh01 <anikethgireesh@gmail.com>
2020-08-27 00:34:40 +0530

    DASH client: download the segments with rate selection from abr algorithms

--- clients/dash_client.py
+++ clients/dash_client.py
@@ -1,6 +1,9 @@
 import asyncio
 import json
+import time
 import os
+from glob import glob
+from pprint import pprint
 
 from collections import namedtuple
 from queue import Queue
@@ -20,7 +23,14 @@ import config
 adaptiveInfo = namedtuple("AdaptiveInfo",
                           'segment_time bitrates segments')
 downloadInfo = namedtuple("DownloadInfo",
-                          'index url quality resolution size downloaded time')
+                          'index file_name url quality resolution size downloaded time')
+
+
+def segment_download_info(manifest, fname, size, idx, url, quality, resolution, time):
+    if size <= 0:
+        return downloadInfo(index=idx, file_name=None, url=None, quality=quality, resolution=resolution, size=0, downloaded=0, time=0)
+    else:
+        return downloadInfo(index=idx, file_name=fname, url=url, quality=quality, resolution=resolution, size=size, downloaded=size, time=time)
 
 
 async def perform_download(configuration: QuicConfiguration, args) -> None:
@@ -67,6 +77,7 @@ class DashClient:
 		self.totalBuffer = args.buffer_size
 		self.currBuffer = 0
 		self.abr_algorithm = None
+		self.segment_baseName = None
 
 		self.lastDownloadSize = 0
 		self.lastDownloadTime = 0
@@ -117,20 +128,91 @@ class DashClient:
 		# returns throughput value of last segment downloaded in kbps
 		return self.latest_tput
 	
-	def fetchNextSegment(self, bitrate = 0):
-		pass
+	async def fetchNextSegment(self, segment_list, bitrate = 0):
+		if not bitrate:
+			return
 
-	
-	async def download_segment(self):
-		pass
+		segment_Duration = 0
+
+		for i, b in enumerate(self.manifest_data['bitrates_kbps']):
+			if b == bitrate:
+				segment_Duration = self.manifest_data['segment_duration_ms'] / self.manifest_data['timescale']
+				break
+
+		for fname in sorted(glob(segment_list)):
+			_, self.segment_baseName = fname.rsplit('/', 1)
+			self.args.urls[0] = self.baseUrl + '/' + str(os.stat(fname).st_size)
+			start = time.time()
+			res = await perform_download(self.configuration, self.args)
+			elapsed = time.time() - start
+
+		data = res[0][0]
+		if data is not None:
+			self.lastDownloadTime = elapsed
+			self.lastDownloadSize = data
+			self.latest_tput =  res[0][1]
+
+			self.segmentQueue.put(self.segment_baseName)
+
+			# QOE parameters update
+			self.perf_parameters['bitrate_change'].append((self.currentSegment + 1,  bitrate))
+			self.perf_parameters['tput_observed'].append((self.currentSegment + 1,  res[0][1]))
+			self.perf_parameters['avg_bitrate'] += bitrate
+			self.perf_parameters['avg_bitrate_change'] += abs(bitrate - self.perf_parameters['prev_rate'])
+
+			if not self.perf_parameters['prev_rate'] or self.perf_parameters['prev_rate'] != bitrate:
+				self.perf_parameters['prev_rate'] = bitrate
+				self.perf_parameters['change_count'] += 1
+
+			self.currentSegment += 1
+			async with self.lock:
+					self.currBuffer += segment_Duration
+
+			ret = True
+		else:
+			print("Error: downloaded segment is none!! Playback will stop shortly")
+			ret = False
+		return ret
+
+	async def download_segment(self) -> None:
+		if config.NUM_SERVER_PUSHED_FRAMES is not None:
+			next_segment_idx = config.NUM_SERVER_PUSHED_FRAMES + 1
+		else:
+			next_segment_idx = 1
+
+		while self.currentSegment + 1 < self.totalSegments:
+			async with self.lock:
+				currBuff = self.currBuffer
+			segment_Duration = int(self.manifest_data['segment_duration_ms']) / int(self.manifest_data['timescale'])
+
+			playback_stats = {}
+			playback_stats["lastTput_kbps"] = self.latest_segment_Throughput_kbps()
+			playback_stats["currBuffer"] = currBuff
+			playback_stats["segment_Idx"] = self.currentSegment + 1
+
+			if self.totalBuffer - currBuff >= segment_Duration:
+				rateNext = self.abr_algorithm.NextSegmentQualityIndex(playback_stats)
+				while next_segment_idx <= len(self.manifest_data['segment_size_bytes']):
+					segment_resolution = self.manifest_data['resolutions'][rateNext]
+					fName = "htdocs/dash/" + segment_resolution + "/out/frame-" + str(next_segment_idx) + "-" + segment_resolution + "-*"
+					if await self.fetchNextSegment(fName, rateNext):
+						dp = segment_download_info(self.manifest_data, self.segment_baseName, self.lastDownloadSize, next_segment_idx, self.args.urls, rateNext, segment_resolution, self.lastDownloadTime)
+						pprint(dp)
+					else:
+						break
+					next_segment_idx += 1
+			else:
+				time.sleep(0.5)
+		self.segmentQueue.put("done")
+
 
 	#emulate playback of frame scenario
 	async def playback_frames(self) -> None:
-		pass
+		print("playback seg")
 
 	#emulate decoding the frame scenario
 	async def decode_frames(self) -> None:
-		pass
+		print("decoding seg")
 
 	async def player(self) -> None:
 		await self.dash_client_set_config()
@@ -139,9 +221,10 @@ class DashClient:
 		#TODO: download the segments
 		#TODO: decode the segements
 		#TODO: play back the segments
-		await self.download_segment()
-		await self.decode_frames()
-		await self.playback_frames()
+
+
+		tasks = [self.download_segment(), self.decode_frames(), self.playback_frames()]
+		await asyncio.gather(*tasks)
 
 		self.perf_parameters['avg_bitrate'] /= self.totalSegments
 		self.perf_parameters['avg_bitrate_change'] /= (self.totalSegments - 1)
