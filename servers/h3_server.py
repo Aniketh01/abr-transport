3fa268f666a7a3c0aced834bb1a9825c9cc05b1c
Aniketh01 <anikethgireesh@gmail.com>
2020-09-14 14:26:07 +0530

    Servers: Have a central entry point for servers as we intend to add support for other HTTP versions as well

---
+++ servers/h3_server.py
@@ -0,0 +1,244 @@
+import asyncio
+try:
+    import uvloop
+except ImportError:
+    uvloop = None
+import logging
+import importlib
+import time
+from email.utils import formatdate
+from typing import Callable, Dict, Optional, Union, cast
+
+import aioquic
+from aioquic.tls import SessionTicket
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.h0.connection import H0_ALPN, H0Connection
+from aioquic.h3.connection import H3_ALPN, H3Connection
+from aioquic.h3.events import DataReceived, H3Event, HeadersReceived
+from aioquic.h3.exceptions import NoAvailablePushIDError
+
+from aioquic.quic.events import (
+    DatagramFrameReceived,
+    QuicEvent,
+    ProtocolNegotiated,
+    StreamDataReceived
+)
+
+from protocol.h3.socketFactory import QuicFactorySocket
+
+AsgiApplication = Callable
+HttpConnection = Union[H0Connection, H3Connection]
+application = Callable
+
+SERVER_NAME = "aioquic/" + aioquic.__version__
+
+class HttpRequestHandler:
+    def __init__(
+        self,
+        *,
+        authority: bytes,
+        connection: HttpConnection,
+        protocol: QuicFactorySocket,
+        scope: Dict,
+        stream_ended: bool,
+        stream_id: int,
+        transmit: Callable[[], None],
+    ) -> None:
+        self.authority = authority
+        self.connection = connection
+        self.protocol = protocol
+        self.queue: asyncio.Queue[Dict] = asyncio.Queue()
+        self.scope = scope
+        self.stream_id = stream_id
+        self.transmit = transmit
+
+        if stream_ended:
+            self.queue.put_nowait({"type": "http.request"})
+
+    def http_event_received(self, event: H3Event) -> None:
+        if isinstance(event, DataReceived):
+            self.queue.put_nowait(
+                {
+                    "type": "http.request",
+                    "body": event.data,
+                    "more_body": not event.stream_ended,
+                }
+            )
+        elif isinstance(event, HeadersReceived) and event.stream_ended:
+            self.queue.put_nowait(
+                {"type": "http.request", "body": b"", "more_body": False}
+            )
+
+    async def run_asgi(self, app: AsgiApplication) -> None:
+        await application(self.scope, self.receive, self.send)
+
+    async def receive(self) -> Dict:
+        return await self.queue.get()
+
+    async def send(self, message: Dict) -> None:
+        if message["type"] == "http.response.start":
+            self.connection.send_headers(
+                stream_id=self.stream_id,
+                headers=[
+                    (b":status", str(message["status"]).encode()),
+                    (b"server", SERVER_NAME.encode()),
+                    (b"date", formatdate(time.time(), usegmt=True).encode()),                    
+                ]
+                + [(k, v) for k, v in message["headers"]],
+            )
+        elif message["type"] == "http.response.body":
+            self.connection.send_data(
+                stream_id=self.stream_id,
+                data=message.get("body", b""),
+                end_stream=not message.get("more_body", False),
+            )
+        elif message["type"] == "http.response.push" and isinstance(
+            self.connection, H3Connection
+        ):
+            request_headers = [
+                (b":method", b"GET"),
+                (b":scheme", b"https"),
+                (b":authority", self.authority),
+                (b":path", message["path"].encode()),
+                (b":Push", b"True")
+            ] + [(k, v) for k, v in message["headers"]]
+
+            # send push promise
+            try:
+                push_stream_id = self.connection.send_push_promise(
+                    stream_id=self.stream_id, headers=request_headers
+                )
+            except NoAvailablePushIDError:
+                return
+
+            # fake request
+            cast(HttpServerProtocol, self.protocol).http_event_received(
+                HeadersReceived(
+                    headers=request_headers, stream_ended=True, stream_id=push_stream_id
+                )
+            )
+        self.transmit()
+
+class HttpServerProtocol(QuicFactorySocket):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._handlers: Dict[int, HttpRequestHandler] = {}
+        self._http: Optional[HttpConnection] = None
+        self.quic_client: bool = False
+
+    def http_event_received(self, event: H3Event) -> None:
+        if isinstance(event, HeadersReceived) and event.stream_id not in self._handlers:
+            authority = None
+            headers = []
+            http_version = "0.9" if isinstance(self._http, H0Connection) else "3"
+            raw_path = b""
+            push = b""
+            method = ""
+            protocol = None
+            for header, value in event.headers:
+                if header == b":authority":
+                    authority = value
+                    headers.append((b"host", value))
+                elif header == b":method":
+                    method = value.decode()
+                elif header == b":path":
+                    raw_path = value
+                elif header == b":protocol":
+                    protocol = value.decode()
+                elif header == b":Push":
+                    push = bool(value.decode())
+                elif header and not header.startswith(b":"):
+                    headers.append((header, value))
+
+            if b"?" in raw_path:
+                path_bytes, query_string = raw_path.split(b"?", maxsplit=1)
+            else:
+                path_bytes, query_string = raw_path, b""
+            path = path_bytes.decode()
+
+            if push:
+                self._quic._logger.info("HTTP request %s Push %s", method, path)
+            else:
+                self._quic._logger.info("HTTP request %s %s", method, path)
+
+            # FIXME: add a public API to retrieve peer address
+            client_addr = self._http._quic._network_paths[0].addr
+            client = (client_addr[0], client_addr[1])
+
+            extensions: Dict[str, Dict] = {}
+            if isinstance(self._http, H3Connection):
+                extensions["http.response.push"] = {}
+                scope = {
+                    "client": client,
+                    "extensions": extensions,
+                    "headers": headers,
+                    "http_version": http_version,
+                    "method": method,
+                    "path": path,
+                    "query_string": query_string,
+                    "raw_path": raw_path,
+                    "root_path": "",
+                    "scheme": "https",
+                    "type": "http",
+                }
+                handler = HttpRequestHandler(
+                    authority=authority,
+                    connection=self._http,
+                    protocol=self,
+                    scope=scope,
+                    stream_ended=event.stream_ended,
+                    stream_id=event.stream_id,
+                    transmit=self.transmit,
+                )
+            self._handlers[event.stream_id] = handler
+            asyncio.ensure_future(handler.run_asgi(application))
+        elif (
+            isinstance(event, (DataReceived, HeadersReceived))
+            and event.stream_id in self._handlers
+        ):
+            handler = self._handlers[event.stream_id]
+            handler.http_event_received(event)
+
+    def quic_event_received(self, event: QuicEvent) -> None:
+        if isinstance(event, ProtocolNegotiated):
+            if event.alpn_protocol.startswith("h3-"):
+                self._http = H3Connection(self._quic)
+            elif event.alpn_protocol.startswith("hq-"):
+                self._http = H0Connection(self._quic)
+            elif event.alpn_protocol.startswith("quic"):
+                self.quic_client = True
+
+
+        if isinstance(event, DatagramFrameReceived):
+            if event.data == b'quic':
+                self._quic.send_datagram_frame(b'quic-ack')
+
+        if isinstance(event, StreamDataReceived):
+            if self.quic_client is True:
+                print(f"print event {event.data}")
+                data = b'quic stream-data recv'
+                end_stream = False
+                self._quic.send_stream_data(event.stream_id, data, end_stream)
+            else:
+                #TODO: Yet to handle a http_client streamDataReceived event
+                pass
+
+        #  pass event to the HTTP layer
+        if self._http is not None:
+            for http_event in self._http.handle_event(event):
+                self.http_event_received(http_event)
+
+
+class SessionTicketStore:
+    """
+    Simple in-memory store for session tickets.
+    """
+
+    def __init__(self) -> None:
+        self.tickets: Dict[bytes, SessionTicket] = {}
+
+    def add(self, ticket: SessionTicket) -> None:
+        self.tickets[ticket.ticket] = ticket
+
+    def pop(self, label: bytes) -> Optional[SessionTicket]:
+        return self.tickets.pop(label, None)
